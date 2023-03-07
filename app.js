c809852321dd408bd1af39906b2290465a86d6fa
Rajashekhar V <rajashekharv.rymec@gmail.com>
2023-03-07 18:13:48 +0000

    crud operations uptoAPI-8

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.get("/user/tweets/feed/", AuthenticateToken, async (request, response) => {
   const { payload } = request;
   const { user_id, username, gender } = payload;
   const latestTweetsQuery = `
-  SELECT user.username AS username, tweet.tweet AS tweet, tweet.date_time AS dateTime
+  SELECT user.username AS username,tweet.tweet AS tweet, tweet.date_time AS dateTime
   FROM user INNER JOIN follower ON follower.following_user_id=user.user_id
   INNER JOIN tweet ON tweet.user_id=follower.following_user_id
   WHERE follower.follower_user_id =${user_id}
@@ -126,4 +126,126 @@ app.get("/user/tweets/feed/", AuthenticateToken, async (request, response) => {
   response.send(latestTweet);
 });
 
-//API-4
+//API-4 the list of all names of people whom the user follows
+
+app.get("/user/following/", AuthenticateToken, async (request, response) => {
+  const { payload } = request;
+  const { user_id, username, gender } = payload;
+  const getFollowingNameQuery = `
+    SELECT DISTINCT user.username AS username
+    FROM user INNER JOIN follower ON follower.following_user_id=user.user_id
+    WHERE follower.follower_user_id = ${user_id};`;
+  const allFollowingUserNames = await db.all(getFollowingNameQuery);
+  response.send(allFollowingUserNames);
+});
+
+//API-5 the list of all names of people who follows the user
+
+app.get("/user/followers/", AuthenticateToken, async (request, response) => {
+  const { payload } = request;
+  const { user_id, username, gender } = payload;
+  const getFollowerNamesQuery = `
+    SELECT DISTINCT user.username AS username
+    FROM user INNER JOIN follower ON follower.follower_user_id=user.user_id
+    WHERE follower.following_user_id = ${user_id}`;
+  const followerNames = await db.all(getFollowerNamesQuery);
+  response.send(followerNames);
+});
+
+//API-6 the user requests a tweet of the user he is following, return the tweet, likes count, replies count and date-time
+
+app.get("/tweets/:tweetId/", AuthenticateToken, async (request, response) => {
+  const { payload } = request;
+  const { user_id, username, gender } = payload;
+  const { tweetId } = request.params;
+  const checkTweetIdOfFollowingUserQuery = `
+    SELECT * FROM follower INNER JOIN tweet ON tweet.user_id = follower.following_user_id
+    WHERE follower.follower_user_id =${user_id} AND tweet_id = ${tweetId};`;
+  const isTweetIdPresent = await db.get(checkTweetIdOfFollowingUserQuery);
+  if (isTweetIdPresent === undefined) {
+    response.status(401);
+    response.send("Invalid Request");
+  } else {
+    const getTweetReplayLikesDetailsQuery = `
+      SELECT tweet.tweet AS tweet, COUNT(like.like_id) AS likes, COUNT(reply.reply_id) AS replies, tweet.date_time AS dateTime
+      FROM follower INNER JOIN tweet ON tweet.user_id =follower.following_user_id
+      INNER JOIN reply ON reply.tweet_id = tweet.tweet_id INNER JOIN like ON
+      like.tweet_id = reply.tweet_id
+      WHERE follower.follower_user_id =${user_id} AND tweet.tweet_id = ${tweetId};`;
+    const tweetReplayLikesDetailsQuery = await db.get(
+      getTweetReplayLikesDetailsQuery
+    );
+    response.send(tweetReplayLikesDetailsQuery);
+  }
+});
+
+//API-7 If the user requests a tweet of a user he is following, return the list of usernames who liked the tweet
+
+//converting the the given result into as expected output i.e array of names inside object.
+const convert_The_list_of_names_As_Likes_inside_object = (likeUserNames) => {
+  let resultArray = [];
+  for (let eachObject of likeUserNames) {
+    resultArray.push(eachObject.username);
+  }
+  return { likes: resultArray };
+};
+
+app.get(
+  "/tweets/:tweetId/likes/",
+  AuthenticateToken,
+  async (request, response) => {
+    const { payload } = request;
+    const { user_id, username, gender } = payload;
+    const { tweetId } = request.params;
+    const checkTweetIdOfFollowingUserQuery = `
+    SELECT * FROM follower INNER JOIN tweet ON tweet.user_id = follower.following_user_id
+    WHERE follower.follower_user_id =${user_id} AND tweet_id = ${tweetId};`;
+    const isTweetIdPresent = await db.get(checkTweetIdOfFollowingUserQuery);
+    if (isTweetIdPresent === undefined) {
+      response.status(401);
+      response.send("Invalid Request");
+    } else {
+      const getLikesUserNameQuery = `
+      SELECT user.username AS username FROM follower INNER JOIN tweet ON tweet.user_id = follower.following_user_id
+      INNER JOIN like ON like.tweet_id=tweet.tweet_id INNER JOIN user ON
+      user.user_id=like.user_id
+      WHERE follower.follower_user_id =${user_id} AND tweet.tweet_id=${tweetId};`;
+      const likeUserNames = await db.all(getLikesUserNameQuery);
+      const resultObject = convert_The_list_of_names_As_Likes_inside_object(
+        likeUserNames
+      );
+      response.send(resultObject);
+    }
+  }
+);
+
+//API-8  the user requests a tweet of a user he is following, return the list of replies.
+
+app.get(
+  "/tweets/:tweetId/replies/",
+  AuthenticateToken,
+  async (request, response) => {
+    const { payload } = request;
+    const { user_id, username, gender } = payload;
+    const { tweetId } = request.params;
+    const checkTweetIdOfFollowingUserQuery = `
+    SELECT * FROM follower INNER JOIN tweet ON tweet.user_id = follower.following_user_id
+    WHERE follower.follower_user_id =${user_id} AND tweet_id = ${tweetId};`;
+    const isTweetIdPresent = await db.get(checkTweetIdOfFollowingUserQuery);
+    if (isTweetIdPresent === undefined) {
+      response.status(401);
+      response.send("Invalid Request");
+    } else {
+      const getReplyUserNames = `
+        SELECT user.username AS username
+        FROM follower INNER JOIN tweet ON tweet.user_id=follower.following_user_id
+        INNER JOIN reply ON reply.tweet_id=tweet.tweet_id INNER JOIN user ON
+        user.user_id=reply.user_id
+        WHERE follower.follower_user_id=${user_id} AND tweet.tweet_id=${tweetId};`;
+      const replyUserNames = await db.all(getReplyUserNames);
+      response.send({ replies: replyUserNames });
+    }
+  }
+);
+
+//API-9 a list of all tweets of the user
